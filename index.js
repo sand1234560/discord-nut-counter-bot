1ed72e7a831a6ba02f4054c5be594f9ae7d6dad5
sand7n <90265322+sand1234560@users.noreply.github.com>
2022-11-14 09:38:21 +0700

    Added some files
    Main file (index.js) and other chicanery added by npm

---
+++ index.js
@@ -0,0 +1,138 @@
+import DiscordJS, { Intents } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const client = new DiscordJS.Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+})
+
+
+
+client.on('ready', () => {
+    console.log('Letssss fucking go')
+})
+client.on('messageCreate', async (message) => {
+    if (message.content == 'log') {
+        message.delete();
+        let nowDate, susMonth, susYear, totalDayMil, totalDay, Amount, susAmount, susTime,
+    lastMessageSplit, previousTotal, finalTotal, overallCumTime;
+
+        const firstDay = new Date(2021, 7, 10); //Start date
+        const dateCreate = new Date();
+        nowDate = dateCreate.getDate();
+        susMonth = dateCreate.getMonth();
+        susYear = dateCreate.getFullYear();
+        totalDayMil = dateCreate - firstDay; //This is total day in milliseconds.
+        totalDay = Math.floor(totalDayMil / (1000*60*60*24)); //Total day milliseconds convert to days.
+        switch(susMonth) {
+            case 0:susMonth = "January";break;
+            case 1:susMonth = "February";break;
+            case 2:susMonth = "March";break;
+            case 3:susMonth = "April";break;
+            case 4:susMonth = "May";break;
+            case 5:susMonth = "June";break;
+            case 6:susMonth = "July";break;
+            case 7:susMonth = "August";break;
+            case 8:susMonth = "September";break;
+            case 9:susMonth = "October";break;
+            case 10:susMonth = "November";break;
+            case 11:susMonth = "December";break;
+            default:susMonth = "Bro, I think month is broken";break;
+        }     
+        console.log(`\n${nowDate} ${susMonth} ${susYear} (${totalDay} days since starting date)`)
+
+        function AmountConvert(susAmount){
+            switch(susAmount) {
+                case ":zero":Amount = 0;break;
+                case ":one:":Amount = 1;break;
+                case ":two:":Amount = 2;break;
+                case ":three:":Amount = 3;break;
+                case ":four:":Amount = 4;break;
+                case ":five:":Amount = 5;break;
+                case ":six:":Amount = 6;break;
+                case ":seven:":Amount = 7;break;
+                case ":eight:":Amount = 8;break;
+                case ":nine:":Amount = 9;break;
+                default:Amount = "Too much, my man.";break;
+            }
+            Amount += 1;
+            return Amount;
+        }
+
+        function timeConvert(Amount){ 
+            if (Amount == 1) {
+                susTime = "time"
+            } else {
+                susTime = "times"
+            }
+            return susTime;
+        }
+
+        function timeConvertAgain(finalTotal){
+            if (finalTotal == 1){
+                overallCumTime = "time"
+            } else {
+                overallCumTime = "times"
+            }
+            return overallCumTime;
+        }
+       // const messages = await message.channel.messages.fetch({ limit: 2 });
+       // const lastMessage = messages.last();
+
+       setTimeout(() => { message.channel.messages.fetch({ limit: 1 }).then(messages => { //fetch 2s after command
+        let lastMessage = messages.first();
+        // do what you need with lastMessage below
+        lastMessageSplit = lastMessage.content.split(' ');
+        previousTotal = lastMessageSplit[9]; //Previous day total
+        finalTotal = parseInt(previousTotal); //turns previousTotal into int
+        finalTotal+= 1;
+        console.log(`previous total: ${previousTotal}, Today total: ${finalTotal}`);
+
+        let previousDay = lastMessageSplit[1]; 
+        let finalPreviousDay = parseInt(previousDay); //previous date
+
+        susAmount = lastMessageSplit[6];
+        AmountConvert(susAmount);
+        timeConvert(Amount);
+        timeConvertAgain(finalTotal);
+
+        switch (Amount){
+            case 0:susAmount = ":zero:"; break; //sus_Amount -> Amount -> fuck_off (Pretty much sus_Amount b)
+            case 1:susAmount = ":one:";break;
+            case 2:susAmount = ":two:";break;
+            case 3:susAmount = ":three:";break;
+            case 4:susAmount = ":four:";break;
+            case 5:susAmount = ":five:";break;
+            case 6:susAmount = ":six:";break;
+            case 7:susAmount = ":seven:";break;
+            case 8:susAmount = ":eight:";break;
+            case 9:susAmount = ":nine:";break;
+            default:susAmount = "Too much, my man.";break;
+        }
+
+        let dateCompare = totalDay-finalPreviousDay
+        console.log(`Previous date: ${finalPreviousDay}`);
+        console.log(`lastMessageSplit: ${lastMessageSplit}`);
+        console.log(`dateCompare: ${dateCompare}`);
+        console.log(`Amount: ${Amount}`);
+
+        setTimeout(() => {
+            //edit the message if 1.the date is the same as previous'
+            if (dateCompare == 0){ //edit message 1 second after fetching ( 3s after command)
+                lastMessage.edit(`**Day ${totalDay} - ${nowDate} ${susMonth} ${susYear}:** ${susAmount} ${susTime} (Total: ${finalTotal} ${overallCumTime})`)
+                .then(msg => console.log(`Updated the content of a message to ${msg.content}`))
+                .catch(console.error);
+            }
+            //send a new message if 1.the date doesn't match previous' 2. No previous message 
+            else if (dateCompare > 0){
+                message.channel.send({
+                    content: `**Day ${totalDay} - ${nowDate} ${susMonth} ${susYear}:** :one: time (Total: ${finalTotal} ${overallCumTime})`
+                })
+            }
+                        }, 1000);
+      })
+      .catch(console.error);  }, 2000);
+    }
+})
+
+client.login(process.env.TOKEN)
